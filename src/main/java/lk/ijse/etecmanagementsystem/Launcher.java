fa89162a5e589f314cc613ee5dd3516582464089
HasithaWije <hasithawijesinghe2020@gmail.com>
2025-11-22 15:43:13 +0530

    c1

---
+++ src/main/java/lk/ijse/etecmanagementsystem/Launcher.java
@@ -0,0 +1,9 @@
+package lk.ijse.etecmanagementsystem;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module lk.ijse.etecmanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens lk.ijse.etecmanagementsystem to javafx.fxml;
+    exports lk.ijse.etecmanagementsystem;
+}
