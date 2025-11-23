73ea3faacdb591cdf47a8c6d0513c19781a43167
HasithaWije <hasithawijesinghe2020@gmail.com>
2025-11-23 09:42:00 +0530

    h2

---
+++ src/main/java/lk/ijse/etecmanagementsystem/App.java
@@ -0,0 +1,41 @@
+package lk.ijse.etecmanagementsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    private static Scene scene;
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        primaryStage = stage;
+        scene = new Scene(loadFXML("login"), 900, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false); // lock size for login
+        primaryStage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+        // keep login fixed-size, allow resizing for other scenes
+        if (primaryStage != null) {
+            primaryStage.setResizable("login".equals(fxml) ? false : true);
+        }
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
---
+++ src/main/java/lk/ijse/etecmanagementsystem/LoginController.java
@@ -0,0 +1,10 @@
+package lk.ijse.etecmanagementsystem;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class LoginController {
+}
+
